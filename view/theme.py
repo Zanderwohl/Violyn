9b37d1a845cfb53a389f37eb165177721254e8a4
Alexander Lowry <lowry.alex@live.com>
2022-05-17 14:28:07 -0500

    Negative and positive backgrounds.

--- view/theme.py
+++ view/theme.py
@@ -1,10 +1,12 @@
 class Theme:
-    def __init__(self, fg=None, bg=None, tg=None, pos=None, neg=None, hl=None, ll=None):
+    def __init__(self, fg=None, bg=None, tg=None, pos_fg=None, pos_bg=None, neg_fg=None, neg_bg=None, hl=None, ll=None):
         self.fg = (0, 0, 0) if fg is None else fg           # Foreground
         self.bg = (255, 255, 255) if bg is None else bg     # Background
         self.tg = (0, 0, 255) if tg is None else tg         # Third-Ground
-        self.pos = (0, 255, 0) if pos is None else pos      # Positive
-        self.neg = (255, 0, 0) if neg is None else neg      # Negative
+        self.pos_fg = (0, 255, 0) if pos_fg is None else pos_fg      # Positive foreground
+        self.pos_bg = (0, 60, 0) if pos_bg is None else pos_bg      # Positive background
+        self.neg_fg = (255, 0, 0) if neg_fg is None else neg_fg      # Negative foreground
+        self.neg_bg = (60, 0, 0) if neg_bg is None else neg_bg      # Negative background
         self.hl = (50, 50, 50) if hl is None else hl        # Highlight
         self.ll = (200, 200, 200) if ll is None else ll     # Lowlight
 
